be2437c483518dfb27276732263c734a2a68dd22
PaulaPerazzo <mpperazzosampaio@gmail.com>
2023-08-21 19:01:05 -0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,9 @@
-import type { GetServerSideProps, GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react';
-
-
-// export default Home
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const response = await fetch('https://api.github.com/users/PaulaPerazzo/repos');
-  
-//   const data = await response.json();
-//   const repositoryNames = data.map((item) => item.name);
-
-//   return {
-  //     props: {
-//       repositories: repositoryNames,
-//       date: new Date().toISOString(),
-//     }
-//   };
-// };
-
-
-// static server generation (SSG) only works in main branches, not deveĺop
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('https://api.github.com/users/PaulaPerazzo/repos');
-  
-  const data = await response.json();
-  
-  const repositoryNames = data.map((item: { name: any; }) => item.name);
-    
-  return {
-    props: {
-      repositories: repositoryNames,
-      date: new Date().toISOString(),
-    },
-
-    revalidate: 60*60*4,
-  };
-};
+import Home from './Home';
 
 //@ts-ignore
-export default function Home({ repositories, date }) {
-
-  console.log(repositories)
+export default function App() {
 
   return (
-    <>
-      <h1>{date}</h1>
-
-      <ul>
-        {/* @ts-ignore */}
-        {repositories.map((repo) => (
-          <li key={repo}>{repo}</li>
-          ))};
-      </ul>
-    </>
+    <Home />
   );
 };
