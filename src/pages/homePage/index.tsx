409efe27d3c9ede9e4c747a0cfe50da7d24c83ab
jailsonsdsj <jailson.junior36@hotmail.com>
2023-09-05 19:34:29 -0300

    organize folders

---
+++ src/pages/homePage/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+
+
+function homePage() {
+    return (
+        <>
+        <p>Hello, world</p>
+      
+        </>
+    )
+}
+
+export default homePage;
